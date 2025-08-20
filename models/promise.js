6bfd3509f52241f54ac9de6acf803f671996d479
UserDeJs <trabalhosgithubgf@gmail.com>
2025-08-20 17:25:09 +0000

    gay

--- models/promise.js
+++ models/promise.js
@@ -4,6 +4,24 @@ const terminal = readlife.createInterface({
   output: process.stdout
 });
 
-terminal.question("Digite o nome do arquivo: \n", function(valor) {
-  console.log("Seja Bem Vindo "+ valor);
+
+// terminal.question("qual seu nome: \n",ction (valor){
+//   console.log("seja bem vindo" + valor);function (valor){
+//   console.log("seja bem vindo" + valor);
+// });
+
+function question(pergunta) {
+  return new Promise(function (resolve, reject){
+    terminal.question(pergunta + "\n", function(valor){
+      resolve(valor)
+    });
+  });
+}
+question("qual seu nome")
+.then(function(valor){
+  console.log(`seja bem vindo ${valor}`); 
+}).catch(function(erro){
+  console.log(`Ocorreu um erro: ${erro}`);
+}).finally(function (){
+  terminal.close();
 });

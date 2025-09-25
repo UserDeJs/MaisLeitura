6817bf653336e485ec6fc0247dd0033f4f1cc789
UserDeJs <trabalhosgithubgf@gmail.com>
2025-09-25 14:30:43 +0000

    Ediro

--- infra/database.js
+++ infra/database.js
@@ -1,10 +1,17 @@
-const { Client } = require("pg")
+const { Client } = require("pg");
 
-function query(sql){
+async function query(sql, args = []) {
   const client = new Client({
-    host: "",
-    port: 2584,
-    user: "",
-    password: "",
-  })
+    host: "localhost",
+    port: 5432,
+    user: "Gustavo",
+    password: "1324",
+  });
+  await client.connect();
+
+  const res = await client.query(sql, args);
+
+  await client.end();
+  return res.rows;
 }
+module.exports = { query };

60c717ab6a111ebc85e29c77b9b45ddb9f6ffa48
UserDeJs <trabalhosgithubgf@gmail.com>
2025-10-22 17:02:07 +0000

    Salvando

---
+++ inframigrations/1761151866815_criar-tabela-li.js
@@ -0,0 +1,35 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {};
+
+pgm.sql(`
+CREATE TABLE livro(
+  
+  numrero_paginas INTEGER,
+  autor VARCHAR(255),
+  titulo VARCHAR(100) NOT NULL,
+  genero VARCHAR(100) NOT NULL,
+  isbm TEXT,
+  resumo TEXT,
+  imagem_capa TEXT,
+  editora VARCHAR(250),
+  disponibilidade BOOLEAN DEFAULT TRUE,
+  tags TEXT,
+  preco NUMERIC(10,2) DEFAULT 0.0
+
+)
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {};

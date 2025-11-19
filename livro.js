b5de5c407bd2a245bfae64d24e0c451238049b4c
UserDeJs <trabalhosgithubgf@gmail.com>
2025-11-19 17:42:35 +0000

    Salvando

--- livro.js
+++ livro.js
@@ -11,10 +11,16 @@ async function consultartops(){
    const resultado = await query(sql);
     return resultados;    
 }
+
 async function consultarmaisbuscados(){
    const sql = 'SELECT * FROM livro WHERE livros_mais_buscados = true;'
    const resultado = await query(sql);
     return resultados;    
 }
+async function consultarCurtidas(){
+   const sql = 'SELECT * FROM livro WHERE curtidas = 0;'
+   const resultado = await query(sql);
+    return resultados;    
+}
 
-module.exports = { consultar, consultartops, consultarmaisbuscados };
+module.exports = { consultar, consultartops, consultarmaisbuscados, consultarCurtidas };
